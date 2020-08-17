c6a677c36347100e363315b772884370ce577d9b
Colin Jaffe <balloonasaurus@gmail.com>
2020-08-17 11:48:55 -0400

    Initial commit of new version.

---
+++ main.js
@@ -0,0 +1,89 @@
+// Remember that our list is called `todos`, and it's in `todos.js`. It's not in this file, nor is it being `require`-d in, but it's available globally because its file is loaded in before this one in `index.html`.
+
+// Write the function below its comment and you'll be doing pretty well.  The printTodo function has been begun for you so that we can drill down in the comments on its individual steps. Add the code for each part below its comment as well.
+// Doing it this way will self-document your code and, more importantly, make it easier to follow along with the assignment!
+
+
+// Add an item to our todo list.
+
+
+// Remove an item at a given index from our todo list. You can use splice!
+
+
+// Given an todo object, put it on the DOM. This is a pretty big function!
+const printTodo = function(todo) {
+  // Use `document.createElement` to make an <li>, and set its text (preferably using `.innerText`) to be our given object's text field.
+
+
+
+  // Query the ul and put it in a variable.
+
+
+
+  // Append the li we made to the ul as the last child using `.appendChild`. If this isn't working for you, check what is being appended to what!
+
+
+
+  // Give our new li a `todo-item` class using `classList`.
+
+
+
+  // Give our new li an id that is the object's id. This is so that we have a matching relationship between todo node elements and their corresponding objects.
+
+
+
+  // Give the li a `complete` class if the todo object indicates it was complete already.
+
+
+
+  // Give the <p> with the todo's text in it an event listener
+  // to toggle that todo's completeness.
+  // This is quite a challenge, so feel free to come back to this one at the end!
+  // You'll want to add an event listener to the `li` you just made, and in that event listener function, toggle its completeness on both the DOM (using `classList.toggle`) and in our global array (toggling its completeness property).
+  // The hard part will be finding it on the DOM and finding it in our array. We can tell what `li` was clicked using the `event` property passed in, and we can tell what object it goes to using the node element's id that we added above.
+
+
+}
+
+
+// Print all todos. Loop through our todos array and call the above function on each one.
+
+
+
+// Call the above function immediately after you define it, so our todos array gets printed out on page load.
+
+
+
+
+// Clear all todos from the DOM. This is a great helper function for refreshing our todos.
+// Test it in the console and see if your lis disappear!
+
+
+
+// Refresh our page by calling each of the two above functions. Since printing all todos checks our todos array, if we make a change to our todos array, we can make our DOM match by simply clearing it and repopulating it according to our todos' new state.
+
+
+
+
+/*
+
+Let's wire it all together. Add an event listener for the add todo button that will:
+1. Queries the input box. We will need that node element again, so save it to a variable!
+2. Create a todo object. Its text should be the text that was in the input box (you might have to research this!), its priority should be set to 2 (we'll return to this when we follow up on this project), and its completeness should be false, as we definitely haven't completed the todo yet.
+3. Pass that object to your adding todos function to put it in our array.
+4. Pass that object to your adding todos function to put it on the DOM.
+5. Stretch goal: remove all text from the input box. Try it without this first, you'll see why we should do it!
+
+*/
+
+
+
+/* 
+
+Wire up your clear todos button. Give it an event listener that clears all todos from the dom (we have a function for that!) and removes them all todo objects from the todos array as well.
+
+*/
+
+
+
+// And you're DONE with a basic todo app! Next we'll make it very, very powerful.
---
+++ todos.js
@@ -0,0 +1,47 @@
+const todos = [
+  {
+    text: 'Christmas shopping',
+    complete: false,
+    priority: 1,
+    id: 0,
+  },
+
+  {
+    text: `make doctor's appointment`,
+    complete: true,
+    priority: 2,
+    id: 1,
+  },
+
+  {
+    text: 'binge watch The Office',
+    complete: false,
+    priority: 2,
+    id: 2,
+  },
+
+  {
+    text: 'research dinosaurs',
+    complete: false,
+    priority: 1,
+    id: 3,
+  },
+
+  {
+    text: 'fire Colin',
+    complete: true,
+    priority: 2,
+    id: 4,
+  },
+
+  {
+    text: 'start novel',
+    complete: true,
+    priority: 1,
+    id: 5,
+  },
+]
+
+let currentTodos = [...todos]
+
+module.exports = todos;
